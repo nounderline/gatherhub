54a24eeddd34fbea97f6588d4d710c23c34a545b
Ralf Gutkowski <rg@libred.org>
2022-09-03 15:45:01 +0200

    Nicer UI views

--- frontend/src/NewsPage.tsx
+++ frontend/src/NewsPage.tsx
@@ -9,6 +9,8 @@ import {
   useArweaveTransactions,
 } from './arweave'
 import NewsForm from './NewsForm'
+import PageHeadline from './PageHeadline'
+import { PlusIcon } from '@heroicons/react/20/solid'
 
 export default ({}) => {
   const { data, error } = useArweaveTransactions(
@@ -29,9 +31,25 @@ export default ({}) => {
 
   return (
     <>
+      <PageHeadline
+        icon="📣"
+        title="Announcements"
+        subtitle="Be in the loop about what's happening."
+      />
+
+      <div className="text-center mt-8">
+        <button
+          type="button"
+          className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+        >
+          <PlusIcon className="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
+          New Announcement
+        </button>
+        <div className=" text-gray-400">for owners only</div>
+      </div>
+
       <div
         onClick={() => {
-          console.log(arweave)
           arweaveWebWallet.connect([
             'ACCESS_ADDRESS',
             'SIGN_TRANSACTION',
@@ -42,7 +60,9 @@ export default ({}) => {
         News
       </div>
 
-      <NewsForm onSubmit={handlePostSubmit} />
+      <Slideover title="New Announcement">
+        <NewsForm onSubmit={handlePostSubmit} />
+      </Slideover>
 
       <div className="m-auto max-w-3xl">
         {data && (
@@ -74,5 +94,89 @@ const Post = ({ data, owner, timestamp }) => {
       <div className=" text-gray-400">{timeString}</div>
       <div className="text-2xl">{data}</div>
     </div>
+  )
+}
+
+import { Fragment } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
+import { XMarkIcon } from '@heroicons/react/24/outline'
+
+function Slideover({ title = '', children }) {
+  const [open, setOpen] = useState(true)
+
+  return (
+    <Transition.Root show={open} as={Fragment}>
+      <Dialog as="div" className="relative z-10" onClose={setOpen}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-in-out duration-500"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in-out duration-500"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 overflow-hidden">
+          <div className="absolute inset-0 overflow-hidden">
+            <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
+              <Transition.Child
+                as={Fragment}
+                enter="transform transition ease-in-out duration-500 sm:duration-700"
+                enterFrom="translate-x-full"
+                enterTo="translate-x-0"
+                leave="transform transition ease-in-out duration-500 sm:duration-700"
+                leaveFrom="translate-x-0"
+                leaveTo="translate-x-full"
+              >
+                <Dialog.Panel className="pointer-events-auto w-screen max-w-2xl">
+                  <div className="flex h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl">
+                    <div className="px-4 sm:px-6">
+                      <div className="flex items-start justify-between">
+                        <Dialog.Title className="text-lg font-medium text-gray-900">
+                          {title}
+                        </Dialog.Title>
+
+                        <div className="ml-3 flex h-7 items-center">
+                          <button
+                            type="button"
+                            className="rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                            onClick={() => setOpen(false)}
+                          >
+                            <span className="sr-only">Close panel</span>
+                            <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="relative mt-6 flex-1 px-4 sm:px-6">
+                      {children}
+                    </div>
+                    <div className="flex flex-shrink-0 justify-end px-4 py-4">
+                      <button
+                        type="button"
+                        className="rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                        onClick={() => setOpen(false)}
+                      >
+                        Cancel
+                      </button>
+                      <button
+                        type="submit"
+                        className="ml-4 inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                      >
+                        Save
+                      </button>
+                    </div>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
   )
 }
---
+++ frontend/src/PageHeadline.tsx
@@ -0,0 +1,9 @@
+export default ({ title, subtitle, icon = '' }) => {
+  return (
+    <div className="text-center">
+      <p className="hidden text-7xl select-none">{icon}</p>
+      <p className="mt-2 text-4xl font-bold tracking-tight">{title}</p>
+      <p className="mt-2 text-base text-gray-500">{subtitle}</p>
+    </div>
+  )
+}
